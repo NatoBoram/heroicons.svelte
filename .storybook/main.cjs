bb56e32c70e7998712e6a6788b76cb617912d1b7
Nato Boram <nato.boram@gmail.com>
2023-01-07 16:26:59 -0500

    🎨 Prettier config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require("path")
 module.exports = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
 	],
 	framework: {
-		name: '@storybook/sveltekit',
-		options: {}
+		name: "@storybook/sveltekit",
+		options: {},
 	},
 	docs: {
-		autodocs: 'tag'
-	}
-};
+		autodocs: "tag",
+	},
+}
