8dac03a0e1318647064a694335dbe4a8c5b509be
Nato Boram <nato.boram@gmail.com>
2023-01-08 20:40:26 -0500

    ➕ Import heroicons

--- scripts/build_heroicons.ts
+++ scripts/build_heroicons.ts
@@ -1,12 +1,14 @@
-import { cpSync, mkdirSync, readdirSync, readFileSync, rmSync, writeFileSync } from 'fs'
+import { cp, mkdir, readFile, readdir, rm, writeFile } from 'fs/promises'
 import { execSync } from 'node:child_process'
 import { join } from 'path'
 
 // Copy heroicons
-execSync('git -C ../../tailwindlabs/heroicons pull')
-rmSync('src/lib/heroicons', { recursive: true })
-mkdirSync('src/lib/heroicons')
-cpSync('../../tailwindlabs/heroicons/optimized/', 'src/lib/heroicons/', { recursive: true })
+await rm('src/lib/heroicons', { recursive: true })
+await mkdir('src/lib/heroicons')
+await Promise.all([
+	cp('node_modules/heroicons/20', 'src/lib/heroicons/20', { recursive: true }),
+	cp('node_modules/heroicons/24', 'src/lib/heroicons/24', { recursive: true }),
+])
 
 function modulify(file: string): string {
 	return file
@@ -21,53 +23,56 @@ function namify(file: string): string {
 	return file.replace('.svg', '').replace('.svelte', '')
 }
 
-function sveltify(dir: string, className: string) {
-	const svgFiles = readdirSync(dir)
+async function sveltify(dir: string, className: string) {
+	const svgFiles = await readdir(dir)
 
 	// SVG to Svelte
 	for (const file of svgFiles) {
 		const path = join(dir, file)
 
-		const svg =
-			`<script lang="ts">let className = '${className}'; export {className as class}</script>` +
-			readFileSync(path, 'utf8').replace('<svg', '<svg class={className} ')
+		const svg = `<script lang="ts">
+	let className = '${className}'
+	export { className as class }
+</script>
 
-		writeFileSync(`${path.replace('.svg', '')}.svelte`, svg)
-		rmSync(path)
+${(await readFile(path, 'utf8')).replace('<svg', '<svg class={className} ')}`
+
+		await Promise.all([writeFile(`${path.replace('.svg', '')}.svelte`, svg), rm(path)])
 	}
 
 	// Index
-	const svelteFiles = readdirSync(dir)
+	const svelteFiles = await readdir(dir)
 	const imports = svelteFiles.map(file => `import ${modulify(file)} from './${file}'`).join('\n')
 	const exports = `export { ${svelteFiles.map(modulify).join(', ')} }`
-	writeFileSync(join(dir, 'index.ts'), `${imports}\n${exports}`)
+	await writeFile(join(dir, 'index.ts'), `${imports}\n${exports}`)
 }
 
-sveltify('src/lib/heroicons/20/solid', 'w-5 h-5')
-sveltify('src/lib/heroicons/24/outline', 'w-6 h-6')
-sveltify('src/lib/heroicons/24/solid', 'w-6 h-6')
+await Promise.all([
+	sveltify('src/lib/heroicons/20/solid', 'w-5 h-5'),
+	sveltify('src/lib/heroicons/24/outline', 'w-6 h-6'),
+	sveltify('src/lib/heroicons/24/solid', 'w-6 h-6'),
+])
 
-function indexify(dir: string) {
-	const folders = readdirSync(dir)
+async function indexify(dir: string) {
+	const folders = await readdir(dir)
 	const exports = folders.map(folder => `export * as ${modulify(folder)} from './${folder}'`)
-	writeFileSync(join(dir, 'index.ts'), exports.join('\n'))
+	await writeFile(join(dir, 'index.ts'), exports.join('\n'))
 }
 
-indexify('src/lib/heroicons/20')
-indexify('src/lib/heroicons/24')
+await Promise.all([indexify('src/lib/heroicons/20'), indexify('src/lib/heroicons/24')])
 
-function storify(size: number, variant: string, title: string, className: string) {
+async function storify(size: number, variant: string, title: string, className: string) {
 	const svelteDir = join('src', 'lib', 'heroicons', size.toString(), variant)
 	const storiesDir = join('src', 'stories', size.toString(), variant)
-	mkdirSync(storiesDir, { recursive: true })
+	await mkdir(storiesDir, { recursive: true })
 
 	// Build SVG story
-	const svelteFiles = readdirSync(svelteDir).filter(file => file.endsWith('.svelte'))
+	const svelteFiles = (await readdir(svelteDir)).filter(file => file.endsWith('.svelte'))
 	for (const file of svelteFiles) {
 		const modulified = modulify(file)
 		const modulifiedSvelte = `${modulified}Svelte`
 
-		writeFileSync(
+		await writeFile(
 			join(storiesDir, `${file.replace('.svelte', '')}.stories.ts`),
 			`import type { Meta, StoryObj } from '@storybook/svelte'
 import { ${modulified} as ${modulifiedSvelte} } from '../../../lib/heroicons/${size}/${variant}'
@@ -86,7 +91,7 @@ export const ${modulified}: Story = {}`,
 	}
 
 	// Build Icon Gallery story
-	writeFileSync(
+	await writeFile(
 		join('src', 'stories', `${title}.mdx`),
 		`import { Meta, Title, IconGallery, IconItem } from '@storybook/addon-docs'
 import { ${svelteFiles
@@ -109,12 +114,16 @@ ${svelteFiles
 	)
 }
 
-rmSync(join('src', 'stories', '20'), { recursive: true, force: true })
-rmSync(join('src', 'stories', '24'), { recursive: true, force: true })
+await Promise.all([
+	rm(join('src', 'stories', '20'), { recursive: true, force: true }),
+	rm(join('src', 'stories', '24'), { recursive: true, force: true }),
+])
 
-storify(20, 'solid', 'Mini', 'w-5 h-5')
-storify(24, 'outline', 'Outline', 'w-6 h-6')
-storify(24, 'solid', 'Solid', 'w-6 h-6')
+await Promise.all([
+	storify(20, 'solid', 'Mini', 'w-5 h-5'),
+	storify(24, 'outline', 'Outline', 'w-6 h-6'),
+	storify(24, 'solid', 'Solid', 'w-6 h-6'),
+])
 
 execSync('pnpm format')
 execSync('pnpm package')
