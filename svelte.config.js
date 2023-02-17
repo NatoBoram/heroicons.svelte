711e07018a64537b71d1b5ed7748aa3b9d86f349
Nato Boram <nato.boram@gmail.com>
2023-02-17 01:41:04 -0500

    ⬆️ Upgrade deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapterAuto from '@sveltejs/adapter-auto'
+import adapterStatic from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+
+function adapter() {
+	if (process.env.GITHUB_ACTIONS) return adapterStatic({ fallback: '404.html' })
+	return adapterAuto()
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({ postcss: true }),
 
 	kit: {
 		adapter: adapter(),
