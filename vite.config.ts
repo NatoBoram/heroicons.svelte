26f7a834d26f212a302455f75cc968dddc2cfcf4
Nato Boram <nato.boram@gmail.com>
2025-02-15 00:13:50 -0500

    🔧 Upgrade TS config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
+const config: UserConfig = defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}', 'scripts/**/*.{test,spec}.{js,ts}'],
 	},
 })
+
+export default config
