f686ea9708e80642943f539cca23979baccd80b9
Nato Boram <nato.boram@gmail.com>
2025-02-16 16:01:42 -0500

    🔥 Remove bad imports

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -1,24 +1,6 @@
-import { describe, test } from 'vitest'
-import { Heroicons, Micro, Mini, Outline, Solid } from './index.ts'
+import { test } from 'vitest'
+import { Spinner } from './index.ts'
 
-describe('index', () => {
-	test('Heroicons', ({ expect }) => {
-		expect(Heroicons).toBeDefined()
-	})
-
-	test('Micro', ({ expect }) => {
-		expect(Micro).toBeDefined()
-	})
-
-	test('Mini', ({ expect }) => {
-		expect(Mini).toBeDefined()
-	})
-
-	test('Outline', ({ expect }) => {
-		expect(Outline).toBeDefined()
-	})
-
-	test('Solid', ({ expect }) => {
-		expect(Solid).toBeDefined()
-	})
+test('Spinner', ({ expect }) => {
+	expect(Spinner).toBeDefined()
 })
