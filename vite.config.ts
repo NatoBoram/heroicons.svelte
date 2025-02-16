10a8d4c788e440ae6058ea985f63f13d8accca7a
Nato Boram <nato.boram@gmail.com>
2025-02-16 16:01:42 -0500

    🔥 Remove bad imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import type { UserConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 
 const config: UserConfig = defineConfig({
-	build: { chunkSizeWarningLimit: 1_600 },
 	plugins: [sveltekit(), tailwindcss()],
 	test: { include: ['src/**/*.{test,spec}.{js,ts}', 'scripts/**/*.{test,spec}.{js,ts}'] },
 })
